c01822642808f8ada8ec200680b3586333d39647
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-10 20:06:15 +0400

    init video

--- src/components/landing/tuto-video.tsx
+++ src/components/landing/tuto-video.tsx
@@ -1,6 +1,12 @@
+import ReactPlayer from 'react-player'
+
 const TutoVideo = () => {
   return (
-    <div>tuto-video</div>
+    <div>
+      <div className='flex justify-center'>
+        <ReactPlayer width="75%" height="75dvh" url='https://www.youtube.com/watch?v=6y9rMxzu-HY&pp=ygUSZGFucyB0b24gY3VsIGphbWVs' />
+      </div>
+    </div>
   )
 }
 
