993f8f725f38947e61ec4ab65996dbddbd1eec2e
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-10 19:15:27 +0300

    Merge branches 'main' and 'main' of github.com:thony32/enigmap

--- src/components/landing/tuto-video.tsx
+++ src/components/landing/tuto-video.tsx
@@ -1,13 +1,25 @@
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player"
+import Plyr from "plyr-react"
+import "plyr-react/plyr.css";
 
 const TutoVideo = () => {
-  return (
-    <div>
-      <div className='flex justify-center'>
-        <ReactPlayer width="75%" height="75dvh" url='https://www.youtube.com/watch?v=6y9rMxzu-HY&pp=ygUSZGFucyB0b24gY3VsIGphbWVs' />
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <div className="flex flex-col justify-center">
+                <ReactPlayer
+                    width="75%"
+                    height="75dvh"
+                    url="https://www.youtube.com/watch?v=6y9rMxzu-HY&pp=ygUSZGFucyB0b24gY3VsIGphbWVs"
+                />
+                <Plyr
+                    source={{
+                        type: "video",
+                        sources: [{ src: "/public/assets/videos/Jasmine Thompson   Mad World karaoké.mp4" }],
+                    }} className="rounded-xl"
+                />
+            </div>
+        </div>
+    )
 }
 
 export default TutoVideo
