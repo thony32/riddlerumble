048648b297a8712d994e4348dda2e9edfe9cb712
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-11 21:16:53 +0300

    eritreritra ny ahy

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -3,12 +3,23 @@ import Ranking from "@/components/landing/ranking"
 import TutoVideo from "@/components/landing/tuto-video"
 import { createLazyFileRoute } from "@tanstack/react-router"
 import "plyr-react/plyr.css"
+import { useEffect } from "react"
+import * as edgedb from "edgedb"
 
 export const Route = createLazyFileRoute("/")({
     component: Index,
 })
 
+const client = edgedb.createHttpClient()
+
 function Index() {
+    useEffect(() => {
+        const fetch = async () => {
+            const result = await client.querySingle(`select Room { * }`)
+            console.log(result)
+        }
+        fetch()
+    }, [])
     return (
         <div>
             <Heading />
