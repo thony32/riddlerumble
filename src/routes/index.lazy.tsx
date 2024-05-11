5ac98d45f2a90f31262a2b779f8956307a1eca5d
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-12 02:13:22 +0400

    fuck ny edgeDB

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -7,9 +7,6 @@ import { createHttpClient } from "edgedb";
 import { useEffect } from "react"
 // import { createClient } from "";
 
-const client = createHttpClient({
-    dsn:"edgedb://edgedb:wT37EZ7LqMYfj9E9G2zfVhXA@enigmap--codeipsum.c-82.i.aws.edgedb.cloud:10700/main"
-});
 
 export const Route = createLazyFileRoute("/")({
     component: Index,
@@ -19,11 +16,26 @@ export const Route = createLazyFileRoute("/")({
 
 function Index() {
     useEffect(() => {
-        const fetchData = async () => {
-            const result = await client.querySingle(`select 1 + 1`);
-            console.log(result);
+        function fetchData() {
+            const query = encodeURIComponent("select Post {*}");
+            const url = `https://enigmap--codeipsum.c-82.i.aws.edgedb.cloud:5656/branch/main/edgeql?query=${query}`;
+
+            fetch(url, {
+                method: "GET",
+                headers: {
+                    "Authorization": "Bearer nbwt1_eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlZGIuZC5hbGwiOnRydWUsImVkYi5pLmFsbCI6dHJ1ZSwiZWRiLnIuYWxsIjp0cnVlLCJpYXQiOjE3MTU0NjQyOTAsImlzcyI6ImF3cy5lZGdlZGIuY2xvdWQiLCJqdGkiOiJuX2RjLUFfZ0VlLXYybGZWQkF0NmhnIiwic3ViIjoienhGSHdnNDVFZS1iVUxNYS05bmdBZyJ9.Qq9Amq5Gg-hJtAQdVsw72XdsOdKdSja6qwzGe21Upy-hHr9hK0J9k4hh7QMFzNSXavOMoMbatY8apVRd_JRuzg",
+                    "Content-Type": "application/json"
+                },
+            })
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.error("Error:", error));
         }
+
+        // Call the function to fetch the data
         fetchData();
+
+
     }, [])
     return (
         <div>
