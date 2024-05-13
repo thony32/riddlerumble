ca34da694e57e83a11a3766c72d9ba193e1f379b
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-13 23:01:00 +0300

    mety amzay ty build ty eee

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,39 +1,39 @@
-import { createClient } from "edgedb"
-import e from "@/dbschema/edgeql-js"
-import Link from "next/link"
+// import { createClient } from "edgedb"
+// import e from "@/dbschema/edgeql-js"
+// import Link from "next/link"
 
-const client = createClient({
-    instanceName: process.env.EDGEDB_INSTANCE,
-    secretKey: process.env.EDGEDB_SECRET_KEY,
-})
+// const client = createClient({
+//     instanceName: process.env.EDGEDB_INSTANCE,
+//     secretKey: process.env.EDGEDB_SECRET_KEY,
+// })
 
-export default async function Post({ params }: { params: { id: string } }) {
-    const post = await e
-        .select(e.Post, (post) => ({
-            id: true,
-            title: true,
-            content: true,
-            filter_single: e.op(post.id, "=", e.uuid(params.id)),
-        }))
-        .run(client)
+// export default async function Post({ params }: { params: { id: string } }) {
+//     const post = await e
+//         .select(e.Post, (post) => ({
+//             id: true,
+//             title: true,
+//             content: true,
+//             filter_single: e.op(post.id, "=", e.uuid(params.id)),
+//         }))
+//         .run(client)
 
-    if (!post) {
-        return <div>Post not found</div>
-    }
+//     if (!post) {
+//         return <div>Post not found</div>
+//     }
 
-    return (
-        <div className="container mx-auto p-4 bg-black text-white">
-            <nav>
-                <Link
-                    href="/"
-                    className="text-blue-500 mb-4 block"
-                    replace
-                >
-                    Back to list
-                </Link>
-            </nav>
-            <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
-            <p>{post.content}</p>
-        </div>
-    )
-}
+//     return (
+//         <div className="container mx-auto p-4 bg-black text-white">
+//             <nav>
+//                 <Link
+//                     href="/"
+//                     className="text-blue-500 mb-4 block"
+//                     replace
+//                 >
+//                     Back to list
+//                 </Link>
+//             </nav>
+//             <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+//             <p>{post.content}</p>
+//         </div>
+//     )
+// }
