ba2ed8aae2de8cb4733aa0f3fb5f62b29b6b8cfa
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-05-13 21:30:29 +0300

    add server function

--- components/button.tsx
+++ components/button.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React from "react"
 
-function ButtonTest() {
-    
+function ButtonTest({ link }: { link: string }) {
+    function handleCLick() {
+        alert(link)
+    }
     return (
         <button
-            onClick={() => alert("kindy!")}
+            onClick={() => handleCLick()}
             className="flex items-center gap-2 bg-blue-500 text-white tracking-wider font-title rounded-lg"
         >
             button
