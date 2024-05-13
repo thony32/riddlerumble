1e0db721620792a9718ba37c687de1783e8455a2
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-13 23:54:28 +0300

    fix: theme handler document.querySelector

--- services/edgedb-action.ts
+++ services/edgedb-action.ts
@@ -1,23 +1,13 @@
-"use server";
-import { Octokit } from "@octokit/core";
-import { BuiltinProviderNames, TokenData } from "@edgedb/auth-nextjs/app";
-import { auth, client } from "@/edgedb";
+"use server"
+import { Octokit } from "@octokit/core"
+import { BuiltinProviderNames, TokenData } from "@edgedb/auth-nextjs/app"
+import { auth, client } from "@/edgedb"
 
 // Auth actions
 
-export const {
-    signout,
-    emailPasswordSignIn,
-    emailPasswordSignUp,
-    emailPasswordSendPasswordResetEmail,
-    emailPasswordResetPassword,
-    emailPasswordResendVerificationEmail,
-} = auth.createServerActions();
+export const { signout, emailPasswordSignIn, emailPasswordSignUp, emailPasswordSendPasswordResetEmail, emailPasswordResetPassword, emailPasswordResendVerificationEmail } = auth.createServerActions()
 
-export async function createUser(
-    tokenData: TokenData,
-) {
-
+export async function createUser(tokenData: TokenData) {
     await client.query(
         `
         with identity := (select ext::auth::Identity filter .id = <uuid>$identity_id),
@@ -27,5 +17,5 @@ export async function createUser(
         {
             identity_id: tokenData.identity_id,
         }
-    );
+    )
 }
