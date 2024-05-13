80fc4c6aace3b381ad11bce58e1c0673f655d055
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-13 20:16:16 +0300

    scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,8 @@
-import type { Config } from "tailwindcss";
-import { nextui } from "@nextui-org/react";
+import type { Config } from "tailwindcss"
+import { nextui } from "@nextui-org/react"
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
-  ],
-  plugins: [nextui(), require('daisyui')],
-};
-export default config;
+    content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+    plugins: [nextui(), require("daisyui")],
+}
+export default config
