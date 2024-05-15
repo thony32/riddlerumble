9ddbd5c33a2b97e13d71fe4531c2d47c6630cea3
tiavinacyri <tiavinacyri@gmail.com>
2024-05-15 14:25:15 +0400

    modal setting

--- components/game/Navbar.tsx
+++ components/game/Navbar.tsx
@@ -1,9 +1,7 @@
-import { auth } from "@/edgedb"
 import getInitial from "@/utils/getInitials"
 import { Avatar } from "@nextui-org/avatar"
-import { Button } from "@nextui-org/button"
-import Link from "next/link"
-
+import BtnLogout from "./BtnLogout"
+import ProfilSetting from "./ProfilSetting"
 const Navbar = async ({ user }: any) => {
     return (
         <div className="flex justify-between items-center">
@@ -11,24 +9,20 @@ const Navbar = async ({ user }: any) => {
                 <h1 className="text-2xl">Enigmap</h1>
             </div>
             <div className="flex gap-8 items-center">
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 relative">
                     <Avatar showFallback name={getInitial(user.full_name)} src={user.avatar} />
                     <div className="flex flex-col text-right">
                         <span>{user.full_name}</span>
                         <span className="font-sans text-xs font-semibold">{user.pseudo}</span>
                     </div>
+                    <div className="absolute -top-2 -left-3 z-50">
+                        <ProfilSetting />
+                    </div>
                 </div>
-                <Link href={auth.getSignoutUrl()}>
-                    <Button className="rounded-full" isIconOnly aria-label="Logout">
-                        <svg fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-                        </svg>
-                    </Button>
-                </Link>
+                <BtnLogout />
             </div>
         </div>
     )
 }
 
 export default Navbar
-export const useClient = true;
