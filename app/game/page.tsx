e3ffbd53581fabb7fd6fe0296e0537552eb1efa1
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-05-16 15:22:54 +0300

    add map component

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,21 +1,23 @@
-import Navbar from "@/components/game/Navbar";
+import Navbar from "@/components/game/Navbar"
 import { auth } from "@/edgedb"
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
-import UserAction from "@/services/user-action";
+import { redirect } from "next/navigation"
+import { cookies } from "next/headers"
+import UserAction from "@/services/user-action"
+import MapGame from "@/components/game/MapGame"
 
 export default async function Game() {
-    const session = auth.getSession();
-    if (await session.isSignedIn() == false) {
-        redirect('/')
+    const session = auth.getSession()
+    if ((await session.isSignedIn()) == false) {
+        redirect("/")
     }
 
-    const identityCookie = cookies().get('identity')?.value as string;
-    const identity = identityCookie.replace(/"/g, '');
-    const user = await UserAction.getData(identity);
+    const identityCookie = cookies().get("identity")?.value as string
+    const identity = identityCookie.replace(/"/g, "")
+    const user = await UserAction.getData(identity)
     return (
         <div>
             <Navbar user={user} />
+            <MapGame />
         </div>
     )
 }
---
+++ components/game/MapGame.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const MapGame = () => {
+    return <div className="w-full h-[100vh]">MapGame</div>
+}
+
+export default MapGame
