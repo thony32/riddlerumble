225b315451be4780e747418e135780ca987aab75
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-16 23:39:59 +0300

    mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: undefined,
+    },
+}
 
-export default nextConfig;
+export default nextConfig
