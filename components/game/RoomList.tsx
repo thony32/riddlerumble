43f24d0b4efdaf1dfb481e618e5eafab96034586
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-05-16 21:19:26 +0300

    add room page

--- components/game/RoomList.tsx
+++ components/game/RoomList.tsx
@@ -1,9 +1,27 @@
 import React from "react"
 
-function RoomList() {
+interface Room {
+    id: string
+    delay: number
+    latitude: string
+    longitude: string
+    nb_players: number
+    prompt: string
+}
+
+interface Props {
+    room_list: Room[]
+}
+
+function RoomList({ room_list }: Props) {
     return (
         <div className="min-h-[100vh] grid grid-cols-3">
-            <div className="col-span-2 bg-gray-200">List of room</div>
+            <div className="col-span-2 bg-gray-200">
+                List of room
+                {room_list.map((room: Room) => (
+                    <div key={room.id}>room : {room.id}</div>
+                ))}
+            </div>
             <div className="bg-blue-200">Trouver room</div>
         </div>
     )
