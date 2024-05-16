f16d154724d82d5ccd86b6471933e0adc632ddba
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-05-16 20:46:07 +0300

    add: mapbox

--- app/game/party/page.tsx
+++ app/game/party/page.tsx
@@ -1,8 +1,29 @@
-import React from "react"
+"use client"
+import Head from "next/head"
+import React, { useState } from "react"
 import Map from "react-map-gl"
 
 function Party() {
-    return <div className="w-full min-h-[100vh]">test</div>
+    return (
+        <div className="w-full py-6 relative overflow-hidden">
+            <Head>
+                <link
+                    href="https://api.tiles.mapbox.com/mapbox-gl-js/v<YOUR_MAPBOX_VERSION>/mapbox-gl.css"
+                    rel="stylesheet"
+                />
+            </Head>
+            <Map
+                initialViewState={{
+                    longitude: -122.4,
+                    latitude: 37.8,
+                    zoom: 4,
+                }}
+                mapboxAccessToken="pk.eyJ1IjoidGhvbnkzMiIsImEiOiJjbHVuNXA2Nm0xYWJpMm1uMjkwb3hndGM4In0.cORWr8oFGZpc9okOvVCKXQ"
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+                style={{ width: "100%", height: "90vh", margin: 0, padding: 0 }}
+            ></Map>
+        </div>
+    )
 }
 
 export default Party
---
+++ app/game/room/page.tsx
@@ -0,0 +1,12 @@
+import RoomList from "@/components/game/RoomList"
+import React from "react"
+
+function Room() {
+    return (
+        <div className="py-6">
+            <RoomList />
+        </div>
+    )
+}
+
+export default Room
