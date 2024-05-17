54691299d2fcfa7d35f6fe98be95026b31c2934b
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-17 23:46:34 +0400

    new loading

--- app/game/layout.tsx
+++ app/game/layout.tsx
@@ -22,7 +22,7 @@ export default function GameLayout({
     })
 
     if (isPending) {
-        return <div>Loading...</div>
+        return <div className="h-screen flex justify-center items-center"><span className="loading loading-ring loading-lg"></span></div>
     }
 
     if (!data) {
