4becda7c67c82add1793c19d04ac73e3150c675f
Notoavina3X <notoavinarazafilalao@gmail.com>
2024-05-20 15:29:09 +0300

    chore: env variables

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -2,10 +2,10 @@ import PusherServer from "pusher"
 import PusherClient from "pusher-js"
 
 export const pusherServer = new PusherServer({
-    appId: "1544547",
-    key: "09e318bef2fb6346410b",
-    secret: "5bebb34e40158a8c971b",
-    cluster: "ap2",
+    appId: process.env.PUSHER_APP_ID!,
+    key: process.env.PUSHER_KEY!,
+    secret: process.env.PUSHER_SECRET!,
+    cluster: process.env.PUSHER_CLUSTER!,
     useTLS: true,
 })
 
