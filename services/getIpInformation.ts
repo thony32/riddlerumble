77a7e89e2e6c6ce33a3014ea872758640d518b10
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-20 18:28:22 +0300

    prod env

--- services/getIpInformation.ts
+++ services/getIpInformation.ts
@@ -1,10 +1,12 @@
-import { IpInformation } from "@/models/ipInformation";
+import { IpInformation } from "@/models/ipInformation"
+
+const IpInfoLink = process.env.NODE_ENV === "production" ? "https://enigmap.vercel.app" : "http://localhost:3000"
 
 export default async function getIpInformation(): Promise<IpInformation> {
-    const response = await fetch('http://localhost:3000/api/getClientNationality');
+    const response = await fetch(`${IpInfoLink}/api/getClientNationality`)
     if (!response.ok) {
-        throw new Error('Failed to fetch IP info');
+        throw new Error("Failed to fetch IP info")
     }
-    const jsonData = await response.json();
-    return jsonData as IpInformation;
+    const jsonData = await response.json()
+    return jsonData as IpInformation
 }
