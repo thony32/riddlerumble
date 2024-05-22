bc2cc80aa9a6b0a5d03c11e3ff8e53007a3c1d32
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-22 21:01:21 +0400

    ip ok

--- pages/api/getClientNationality.ts
+++ pages/api/getClientNationality.ts
@@ -6,7 +6,6 @@ interface LocationResponse {
 }
 
 interface ApiResponse {
-    ip: string
     localisation: LocationResponse
     flag: string
 }
@@ -17,30 +16,28 @@ interface ApiError {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse | ApiError>) {
     try {
-        // const [ipResponse, locationResponse] = await Promise.all([
-        //     fetch("https://api.ipify.org").then((response) => response.text()),
-        //     fetch("http://ip-api.com/json/").then((response) => response.json()),
-        // ])
-
-        // const { countryCode } = locationResponse
-
-        // if (!countryCode) {
-        //     throw new Error("Country code not found")
-        // }
-
-        // const flagResponse = await fetch(`https://flagsapi.com/${countryCode}/flat/64.png`)
-        // if (!flagResponse.ok) {
-        //     throw new Error("Failed to fetch flag")
-        // }
-
-        // const flagUrl = flagResponse.url
-
         const detectedIp = requestIp.getClientIp(req) as any
-        res.status(200).json(
-            detectedIp,
-            // localisation: locationResponse,
-            // flag: flagUrl,
-        )
+        const [locationResponse] = await Promise.all([
+            fetch(`http://ip-api.com/json/${detectedIp}`).then((response) => response.json()),
+        ])
+
+        const { countryCode } = locationResponse
+
+        if (!countryCode) {
+            throw new Error("Country code not found")
+        }
+
+        const flagResponse = await fetch(`https://flagsapi.com/${countryCode}/flat/64.png`)
+        if (!flagResponse.ok) {
+            throw new Error("Failed to fetch flag")
+        }
+
+        const flagUrl = flagResponse.url
+
+        res.status(200).json({
+            localisation: locationResponse,
+            flag: flagUrl,
+        })
     } catch (error) {
         if (error instanceof Error) {
             res.status(500).json({ error: error.message })
