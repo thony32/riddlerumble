d17c808d323435c61b97693e635bcd1a759a5077
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-22 21:49:27 +0400

    fix create user

--- pages/api/getClientNationality.ts
+++ pages/api/getClientNationality.ts
@@ -16,27 +16,15 @@ interface ApiError {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse | ApiError>) {
     try {
-        const detectedIp = requestIp.getClientIp(req) as any
         const [locationResponse] = await Promise.all([
-            fetch(`http://ip-api.com/json/${detectedIp}`).then((response) => response.json()),
+            fetch(`http://ip-api.com/json/${requestIp.getClientIp(req)}`).then((response) => response.json()),
         ])
 
         const { countryCode } = locationResponse
 
-        if (!countryCode) {
-            throw new Error("Country code not found")
-        }
-
-        const flagResponse = await fetch(`https://flagsapi.com/${countryCode}/flat/64.png`)
-        if (!flagResponse.ok) {
-            throw new Error("Failed to fetch flag")
-        }
-
-        const flagUrl = flagResponse.url
-
         res.status(200).json({
             localisation: locationResponse,
-            flag: flagUrl,
+            flag: `https://flagsapi.com/${countryCode}/flat/64.png`,
         })
     } catch (error) {
         if (error instanceof Error) {
