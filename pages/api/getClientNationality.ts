664627ac3e040695a233ce220eafa93681ffd485
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-22 20:44:32 +0400

    test ip

--- pages/api/getClientNationality.ts
+++ pages/api/getClientNationality.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import requestIp from 'request-ip'
 
 interface LocationResponse {
     countryCode?: string
@@ -16,29 +17,30 @@ interface ApiError {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse | ApiError>) {
     try {
-        const [ipResponse, locationResponse] = await Promise.all([
-            fetch("https://api.ipify.org").then((response) => response.text()),
-            fetch("http://ip-api.com/json/").then((response) => response.json()),
-        ])
+        // const [ipResponse, locationResponse] = await Promise.all([
+        //     fetch("https://api.ipify.org").then((response) => response.text()),
+        //     fetch("http://ip-api.com/json/").then((response) => response.json()),
+        // ])
 
-        const { countryCode } = locationResponse
+        // const { countryCode } = locationResponse
 
-        if (!countryCode) {
-            throw new Error("Country code not found")
-        }
+        // if (!countryCode) {
+        //     throw new Error("Country code not found")
+        // }
 
-        const flagResponse = await fetch(`https://flagsapi.com/${countryCode}/flat/64.png`)
-        if (!flagResponse.ok) {
-            throw new Error("Failed to fetch flag")
-        }
+        // const flagResponse = await fetch(`https://flagsapi.com/${countryCode}/flat/64.png`)
+        // if (!flagResponse.ok) {
+        //     throw new Error("Failed to fetch flag")
+        // }
 
-        const flagUrl = flagResponse.url
+        // const flagUrl = flagResponse.url
 
-        res.status(200).json({
-            ip: ipResponse,
-            localisation: locationResponse,
-            flag: flagUrl,
-        })
+        const detectedIp = requestIp.getClientIp(req) as any
+        res.status(200).json(
+            detectedIp,
+            // localisation: locationResponse,
+            // flag: flagUrl,
+        )
     } catch (error) {
         if (error instanceof Error) {
             res.status(500).json({ error: error.message })
