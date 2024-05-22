212d0e982a4832c6c27c02473a382b07a3a1e259
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-05-22 19:12:21 +0300

    .

--- pages/api/getTempRoomPerRoom.ts
+++ pages/api/getTempRoomPerRoom.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next"
-import { createClient } from "@/dbschema/edgeql-js"
+import e, { createClient } from "@/dbschema/edgeql-js"
 import { EDGEDB_INSTANCE, EDGEDB_SECRET_KEY } from "@/env"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -9,25 +9,45 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     })
 
     try {
-        const tempRoomQuery = `
-            select Temp_room {
-                id,
-                latitude,
-                longitude,
-                time,
-                id_user: {
-                  pseudo,
-                  nationality,
-                  avatar
-                },
-              }filter .id_room.id = <uuid>$id_room;
-            `
+        // const tempRoomQuery = `
+        //     select Temp_room {
+        //         id,
+        //         latitude,
+        //         longitude,
+        //         time,
+        //         id_user: {
+        //           pseudo,
+        //           nationality,
+        //           avatar
+        //         },
+        //       }filter .id_room.id = <uuid>$id_room;
+        //     `
+        // const params = {
+        //     id_room: req.body.id_room,
+        // }
+
+        // const response = await client.query(tempRoomQuery, params)
+        // res.status(200).json(response)
+
         const params = {
             id_room: req.body.id_room,
         }
-
-        const response = await client.query(tempRoomQuery, params)
-        res.status(200).json(response)
+        const tempRoomQuery = e.select(e.Temp_room, (temp_room) => ({
+            id: true,
+            latitude: true,
+            longitude: true,
+            time: true,
+            id_user: {
+                pseudo: true,
+                nationality: true,
+                avatar: true,
+            },
+            filter: e.op(temp_room.id_room.id, '=', e.uuid(params.id_room)),
+        }));
+    
+        const response = await tempRoomQuery.run(client);
+    
+        res.status(200).json(response);
     } catch (error) {
         console.log(error)
         res.status(500).json({ success: false, error: error })
