826102d45cb451a104c9089855f1608363bd549a
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-23 00:07:04 +0400

    new sign in sign up

--- app/auth/[...auth]/route.ts
+++ app/auth/[...auth]/route.ts
@@ -1,8 +1,7 @@
 import { redirect } from "next/navigation"
 import { auth } from "@/edgedb"
 import { createUser } from "@/services/edgedb-action"
-import { cookies } from 'next/headers';
-import getIpInformation from "@/services/getIpInformation";
+import { cookies } from "next/headers";
 
 const { GET, POST } = auth.createAuthRouteHandlers({
     async onOAuthCallback({ error, tokenData, provider, isSignUp }) {
@@ -11,8 +10,7 @@ const { GET, POST } = auth.createAuthRouteHandlers({
         }
         cookies().set('identity', JSON.stringify(tokenData.identity_id))
         if (isSignUp) {
-            const ipInformation = await getIpInformation();
-            await createUser(tokenData, ipInformation.localisation.country + ' , ' + ipInformation.localisation.countryCode, provider);
+            await createUser(tokenData, provider);
         }
         redirect("/game")
     },
