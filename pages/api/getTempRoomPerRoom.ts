07664ce2e4902ce3e60efdd99354d7d1bd7eb1a1
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-05-22 20:18:02 +0300

    fix get temp room

--- pages/api/getTempRoomPerRoom.ts
+++ pages/api/getTempRoomPerRoom.ts
@@ -9,45 +9,25 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     })
 
     try {
-        // const tempRoomQuery = `
-        //     select Temp_room {
-        //         id,
-        //         latitude,
-        //         longitude,
-        //         time,
-        //         id_user: {
-        //           pseudo,
-        //           nationality,
-        //           avatar
-        //         },
-        //       }filter .id_room.id = <uuid>$id_room;
-        //     `
-        // const params = {
-        //     id_room: req.body.id_room,
-        // }
 
-        // const response = await client.query(tempRoomQuery, params)
-        // res.status(200).json(response)
+        const { id_room } = req.body;
 
-        const params = {
-            id_room: req.body.id_room,
-        }
         const tempRoomQuery = e.select(e.Temp_room, (temp_room) => ({
             id: true,
             latitude: true,
             longitude: true,
             time: true,
-            id_user: {
+            User: {
                 pseudo: true,
                 nationality: true,
                 avatar: true,
             },
-            filter: e.op(temp_room.id, '=', e.uuid(params.id_room)),
+            filter: e.op(temp_room.Room.id, '=', e.uuid(id_room)),
         }));
-    
-        const response = await tempRoomQuery.run(client);
-    
-        res.status(200).json(response);
+
+        const temp_room = await tempRoomQuery.run(client);
+        res.status(200).json(temp_room);
+
     } catch (error) {
         console.log(error)
         res.status(500).json({ success: false, error: error })
