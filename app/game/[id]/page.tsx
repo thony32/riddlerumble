2a375496a31a3057fd13ab8c0c243b416d1120cf
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-05-24 13:19:02 +0300

    clean

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client"
-import { Avatar, Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react"
+import { Avatar, Button } from "@nextui-org/react"
 import { useMutation, useQuery } from "@tanstack/react-query"
 import { useUser } from "@/store/useUser"
 import { ReactTyped } from "react-typed"
@@ -115,14 +115,6 @@ const Party = ({ params }: { params: { id: string } }) => {
         }
     }
 
-    // const calculateScoreDistance = (distance: number, maxDistance: number = 1750) => {
-    //     distance = Math.min(distance, maxDistance)
-    //     let score = 100 - (distance / maxDistance) * 100
-    //     score = Math.max(0, Math.min(100, score))
-
-    //     return Math.round(score)
-    // }
-
     const user = useUser((state) => state.user)
     const createTempRoom = useMutation({
         mutationKey: ["createTempRoom"],
@@ -163,33 +155,6 @@ const Party = ({ params }: { params: { id: string } }) => {
         },
     })
     const setSelectedRoom = useSelectedRoom((state) => state.setSelectedRoom)
-    // const submitResult = () => {
-    //     const elapsedTime = Date.now() - (startTime || Date.now())
-    //     const elapsedMinutes = Math.floor(elapsedTime / 60000)
-    //     const elapsedSeconds = Math.floor((elapsedTime % 60000) / 1000)
-    //     const formattedElapsedTime = `${String(elapsedMinutes).padStart(2, "0")}:${String(elapsedSeconds).padStart(2, "0")}`
-    //     setElapsedTime(formattedElapsedTime)
-
-    //     const maxTime = 300000
-    //     const timePercentage = (elapsedTime / maxTime) * 100
-    //     const timeScore = Math.round(Math.max(0, 90 - timePercentage * 1.5))
-
-    //     const scoreDistance = calculateScoreDistance(distance)
-
-    //     const goldenRatio = (1 + Math.sqrt(5)) / 2
-    //     const distanceWeight = goldenRatio * 0.3
-    //     const timeWeight = 1 / goldenRatio
-
-    //     const totalScore = Math.round((scoreDistance * distanceWeight + timeScore * timeWeight) / (distanceWeight + timeWeight))
-    //     setShowTarget(true)
-    //     setTotalScore(roomData.level == "high-level" ? totalScore - penalityPoints : totalScore - penalityPoints + 10)
-    //     mapRef.current?.flyTo({ center: [targetMarker.longitude, targetMarker.latitude], duration: 2000, zoom: 5 })
-    //     createTempRoom.mutate()
-    //     createPlayerStat.mutate()
-    //     updateUserScoreMutation.mutate()
-    //     localStorage.removeItem(PARTY_START_TIME_KEY)
-    //     setSelectedRoom(null)
-    // }
 
     const handleSubmitResult = () => {
         const submitParams: SubmitResultParams = {
