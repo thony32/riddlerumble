0b7accd520d8c2aaac04593c3def59cf34a8f845
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-24 22:51:01 +0400

    add key

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -314,7 +314,7 @@ const Party = ({ params }: { params: { id: string } }) => {
                             zoom: 4,
                         }}
                         onClick={clickPostion}
-                        mapboxAccessToken={MAPBOX_TOKEN!}
+                        mapboxAccessToken="pk.eyJ1IjoidGhvbnkzMiIsImEiOiJjbHc5azQ5bWQwNWhjMmtxa2Q5dTcyNWxhIn0.pXpGUWi_9wWY3zwfflmzSQ"
                         style={{ width: "100%", height: "85dvh", margin: 0, padding: 0, borderRadius: "1rem", overflow: "hidden" }}
                     >
                         {!showTarget && (
