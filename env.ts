74a4188d39403aadb2d1400aac5b50d6892624b4
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-25 13:02:33 +0300

    test

--- env.ts
+++ env.ts
@@ -1,9 +1,5 @@
 export const EDGEDB_INSTANCE = process.env.EDGEDB_INSTANCE!
 export const EDGEDB_SECRET_KEY = process.env.EDGEDB_SECRET_KEY!
 export const BASE_URL = process.env.NODE_ENV === "production" ? process.env.BASE_URL! : "http://localhost:3000"
-export const PUSHER_APP_ID = process.env.PUSHER_APP_ID!
-export const PUSHER_KEY = process.env.PUSHER_KEY!
-export const PUSHER_SECRET = process.env.PUSHER_SECRET!
-export const PUSHER_CLUSTER = process.env.PUSHER_CLUSTER!
 export const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN!
 export const SOCKETIO_URL = process.env.SOCKETIO_URL!
