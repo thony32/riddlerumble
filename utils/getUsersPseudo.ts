0b442104f9d980cf68daa0a1ea8455a9c5ed6212
Ndrihanja <ndrihanjarajaonarivony@gmail.com>
2024-05-25 20:19:40 +0300

    track meurtriere

--- utils/getUsersPseudo.ts
+++ utils/getUsersPseudo.ts
@@ -1,4 +1,8 @@
-export default function getUsersPseudo(user_pseudo: string) {
-    console.log("Erreur maivana : ", user_pseudo)
-    return user_pseudo.split(", ").filter(Boolean)
+export default function getUsersPseudo(user_pseudo: string | null) {
+    if (!user_pseudo) {
+        console.log("Erreur maivana : user_pseudo is null or undefined");
+        return [];
+    }
+    console.log("Erreur maivana : ", user_pseudo);
+    return user_pseudo.split(", ").filter(Boolean);
 }
