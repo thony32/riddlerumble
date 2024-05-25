e8aa148335c74ed677446e96423bfd0b6c768382
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-05-26 01:55:08 +0300

    666

--- lib/socket-io.ts
+++ lib/socket-io.ts
@@ -2,4 +2,4 @@ import { SOCKETIO_UPDATE_URL, SOCKETIO_URL } from "@/env"
 import io from "socket.io-client"
 
 export const socket = io(SOCKETIO_URL)
-export const socket_update = io(SOCKETIO_UPDATE_URL)
+export const socket_update = io(SOCKETIO_UPDATE_URL);
