2bea286220fec560abc87c379a2c7843fda4b8c5
TiavinaCyri <tiavinacyri@gmail.com>
2024-05-26 02:31:21 +0400

    build record

--- pages/api/room/setJoker.ts
+++ pages/api/room/setJoker.ts
@@ -29,7 +29,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const updateQuery = e.update(e.Room, () => ({
             filter_single: { id: e.uuid(id) }, // Filter the room by its id
             set: {
-                joker: joker // Update the joker
+                joker: "thony32" // Update the joker
             },
         }))
 
